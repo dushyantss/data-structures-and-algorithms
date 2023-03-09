8c03fc99b20029c4fcf55a5ec957c7e4c7cc82c6
Dushyant Singh Shekhawat <contact@dushyantss.com>
2023-03-09 18:40:49 +0530

    Refactor LCPTree and add unique_substring_count to it

--- ruby/test/data_structures/test_suffix_array.rb
+++ ruby/test/data_structures/test_suffix_array.rb
@@ -15,5 +15,15 @@ module DataStructures
       assert_equal("banana", sa.string)
       assert_equal([5, 3, 1, 0, 4, 2], sa.arr)
     end
+
+    class TestLongestCommonPrefixArray < Minitest::Test
+      def test_all
+        lcpa = ::DataStructures::SuffixArray::LongestCommonPrefixArray.new "ABABBAB"
+
+        assert_equal("ABABBAB", lcpa.string)
+        assert_equal([0, 2, 2, 0, 1, 3, 1], lcpa.arr)
+        assert_equal(19, lcpa.unique_substrings_count)
+      end
+    end
   end
 end
